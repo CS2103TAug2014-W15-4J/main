45833d00a2e1a580dc4dac5416852bc5876ee164
asdoll <ludeqqwo@gmail.com>
2014-09-19 17:10:40 +0800

    build structure for parser
    the structure of parser is mainly build

--- src/controller/parser.java
+++ src/controller/parser.java
@@ -3,28 +3,83 @@ package controller;
 import java.util.*;
 
 public class parser {
-	
-	static Scanner scanner=new Scanner(System.in);
-	private static final String[] CMD_ARRAY={"add","delete","search","edit","display",
-		"redo","undo","clear","tag","done","complete","finish","show",};
-	
-	
-	public static String readLine(){
-		return scanner.nextLine().trim();
-	}
-	
-	public static userInput parse(String input){
-		String command=input.split(" ",2)[0];
-		if(trueCommand(command)){
-			
+	private static final String[] CMD_ARRAY = { "add", "delete", "search",
+			"edit", "display", "redo", "undo", "clear", "tag", "done",
+			"complete", "finish", "show" };
+	private static final String CMD_ADD = "add";
+	private static final String CMD_DELETE = "delete";
+	private static final String CMD_SEARCH = "search";
+	private static final String CMD_EDIT = "edit";
+	private static final String CMD_DISPLAY = "display";
+	private static final String CMD_DONE = "done";
+	private static final String CMD_COMPLETE = "complete";
+	private static final String CMD_FINISH =  "finish";
+	private static final String CMD_SHOW = "show";
+
+	public static userInput parse(String input) {
+		String command = input.split(" ", 2)[0];
+		String content = input.split(" ", 2)[1];
+		switch (command) {
+		case CMD_ADD:
+			return parseAdd(content);
+		case CMD_DELETE:
+			return parseDelete(content);
+		case CMD_SEARCH:
+			return parseSearch(content);
+		case CMD_EDIT:
+			return parseEdit(content);
+		case CMD_DISPLAY:
+			return parseDisplay(content);
+		case CMD_DONE:
+			return parseDone(content);
+		case CMD_COMPLETE:
+			return parseDone(content);
+		case CMD_FINISH:
+			return parseDone(content);
+		case CMD_SHOW:
+			return parseShow(content);
+		default:
+			return errorCommand(content);
 		}
 	}
 
-	private static boolean trueCommand(String command) {
-		for(String i:CMD_ARRAY)
-		if(i.equalsIgnoreCase(command))
-			return true;
-		return false;
+	private static userInput parseAdd(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseDelete(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseSearch(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseEdit(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseShow(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseDone(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput parseDisplay(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static userInput errorCommand(String content) {
+		return null;
 	}
 
 }
