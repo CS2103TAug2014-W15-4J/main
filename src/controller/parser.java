0e614cab3078322e74ce6751af28fee443340321
asdoll <ludeqqwo@gmail.com>
2014-09-19 16:40:03 +0800

    add check command function
    version1.0

--- src/controller/parser.java
+++ src/controller/parser.java
@@ -1,8 +1,30 @@
 package controller;
 
+import java.util.*;
+
 public class parser {
-	public static void main(String[] args){
-		
+	
+	static Scanner scanner=new Scanner(System.in);
+	private static final String[] CMD_ARRAY={"add","delete","search","edit","display",
+		"redo","undo","clear","tag","done","complete","finish","show",};
+	
+	
+	public static String readLine(){
+		return scanner.nextLine().trim();
+	}
+	
+	public static userInput parse(String input){
+		String command=input.split(" ",2)[0];
+		if(trueCommand(command)){
+			
+		}
+	}
+
+	private static boolean trueCommand(String command) {
+		for(String i:CMD_ARRAY)
+		if(i.equalsIgnoreCase(command))
+			return true;
+		return false;
 	}
 
 }
