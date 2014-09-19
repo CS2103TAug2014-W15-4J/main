33accd2d9b3f28f99bc859eca72ed8827f0a8d40
asdoll <ludeqqwo@gmail.com>
2014-09-19 16:52:25 +0800

    update userInput
    add valid of userInput

--- src/controller/userInput.java
+++ src/controller/userInput.java
@@ -5,48 +5,63 @@ import java.util.*;
 
 /**
  * 
- * Object passing from parser to logic
- * The format of time is "yyyy-MM-dd HH:mm"
- * (For command with only one time, default we use addBeginDate and getBeginDate)
+ * Object passing from parser to logic The format of time is "yyyy-MM-dd HH:mm"
+ * (For command with only one time, default we use addBeginDate and
+ * getBeginDate)
+ * 
  * @author Lu Yuehan
  *
  */
 public class userInput {
-	
-	private static String command=null;
-	private static String event=null;
-	private static boolean floating=true;
+	private static boolean valid = true;
+	private static String command = null;
+	private static String event = null;
+	private static boolean floating = true;
 	private static Date beginTime;
 	private static Date endTime;
-	private static SimpleDateFormat timeFormat=new SimpleDateFormat ("yyyy-MM-dd HH:mm"); 
-	
-	public static String getCommand(){
+	private static SimpleDateFormat timeFormat = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm");
+
+	public static boolean getValid() {
+		return valid;
+	}
+
+	public static String getCommand() {
 		return command;
 	}
-	public static String getEvent(){
+
+	public static String getEvent() {
 		return event;
 	}
-	public static boolean isFloat(){
+
+	public static boolean isFloat() {
 		return floating;
 	}
-	
-	public static void add(String userCommand,String userEvent,boolean userFloat){
-		command=userCommand;
-		event=userEvent;
-		floating=userFloat;
+
+	public static void unvalidation() {
+		valid = false;
 	}
 
-	public static void addBeginDate(String beginDate) throws ParseException{
-		beginTime=timeFormat.parse(beginDate);
-	}
-	public static void addEndDate(String endDate) throws ParseException{
-		endTime=timeFormat.parse(endDate);
+	public static void add(String userCommand, String userEvent,
+			boolean userFloat) {
+		command = userCommand;
+		event = userEvent;
+		floating = userFloat;
 	}
 
-	public static Date getBeginDate(){
+	public static void addBeginDate(String beginDate) throws ParseException {
+		beginTime = timeFormat.parse(beginDate);
+	}
+
+	public static void addEndDate(String endDate) throws ParseException {
+		endTime = timeFormat.parse(endDate);
+	}
+
+	public static Date getBeginDate() {
 		return beginTime;
 	}
-	public static Date getEndDate(){
+
+	public static Date getEndDate() {
 		return endTime;
 	}
 }
