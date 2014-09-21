b0d90b9cd03bf2fc28e3c4ab12c53ada88c2a193
Jiang Sheng <gisonrg@gmail.com>
2014-09-21 22:46:04 +0800

    Add a simple task model

---
+++ src/model/Task.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Task {
+	private String description;
+	public Task(String description) {
+		this.description = description;
+	}
+
+	
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	
+	
+}
