26412c2eaa65b57df09ffb3b343d02d0c0d46293
Jiang Sheng <gisonrg@gmail.com>
2014-09-22 01:25:16 +0800

    Model fixed. Add some testing method to TaskList

--- src/model/TaskList.java
+++ src/model/TaskList.java
@@ -1,5 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 public class TaskList {
 
+	@XStreamAlias("TaskList")
+	private ArrayList<Task> tasks;
+
+	public TaskList() {
+		this.tasks = new ArrayList<>();
+	}
+	
+	public ArrayList<Task> getList() {
+		return this.tasks;
+	}
+	
+	@Override
+	public String toString() {
+		String output = "";
+		int i=1;
+		for (Task task : this.tasks) {
+			output+=i;
+			output+=": ";
+			output+= task.getDescription();
+			output+="\n";
+			i++;
+		}
+		return output;
+	}
+	
+	public void test() {
+		for (int i=0;i<50;i++) {
+			this.tasks.add(new Task("No."+i));
+		}
+	}
 }
