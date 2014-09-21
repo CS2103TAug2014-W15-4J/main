7bea620fab31ac4dba31c36e844414570afe9cda
Jiang Sheng <gisonrg@gmail.com>
2014-09-21 22:46:44 +0800

    building storage now

--- src/controller/Storage.java
+++ src/controller/Storage.java
@@ -1,13 +1,75 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import com.thoughtworks.xstream.XStream;
+
+import model.Task;
 import model.TaskList;
 
 public class Storage {
-	public static String save(TaskList list) {
+	
+	private static final String TASK_FILE = "uClear.xml";
+	
+	private static final String ERROR_IO = "Error when trying to read/write the file.";
+	
+	// These two Reader and Writer are used to access and manipulate the given
+	// text file
+	private BufferedReader reader;
+	private BufferedWriter writer;
+	
+	public Storage() {
+		this.initilize();
+	}
+	
+	public String save(TaskList list) {
 		return null;
 	}
 	
-	public static TaskList load() {
+	public TaskList load() {
 		return null;
+	}
+	
+	public String serialize(Task task) {
+		XStream xstream = new XStream();
+		xstream.alias("task", Task.class);
+		String xml = xstream.toXML(task);
+		return xml;
+	}
+	
+	
+	private void initilize() {
+		File inputFile = new File(TASK_FILE);
+		try {
+			if (inputFile.exists()) {
+				this.reader = new BufferedReader(new FileReader(inputFile));
+				this.writer = new BufferedWriter(new FileWriter(inputFile, true));
+			} else {
+				// if the file does not exist, we create a new file
+				this.writer = new BufferedWriter(new FileWriter(inputFile, false));
+				this.reader = new BufferedReader(new FileReader(inputFile));
+			}
+		} catch (Exception e) {
+			throw new Error(ERROR_IO);
+		}
+	}
+	
+	
+	private void readFromFile() {
+		
+	}
+	
+	private  void saveToFile() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Task a = new Task("going to movies tonight");
+		Storage storge = new Storage();
+		System.out.println(serialize(a));
 	}
 }
