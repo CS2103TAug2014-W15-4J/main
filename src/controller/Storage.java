aff40be76e3c67ba1170f7b50de901b3d1fadfbe
Jiang Sheng <gisonrg@gmail.com>
2014-09-21 21:42:20 +0800

    Create Storage class

---
+++ src/controller/Storage.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.TaskList;
+
+public class Storage {
+	public static String save(TaskList list) {
+		return null;
+	}
+	
+	public static TaskList load() {
+		return null;
+	}
+}
