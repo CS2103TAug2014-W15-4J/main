b4e94ee5e0485fa064d5556c668eb8ec5834f209
Jiang Sheng <gisonrg@gmail.com>
2014-09-22 01:23:53 +0800

    close #18 add Save and Load for Storage. Now it is working.

--- src/controller/Storage.java
+++ src/controller/Storage.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -23,31 +22,56 @@ public class Storage {
 	// text file
 	private BufferedReader reader;
 	private BufferedWriter writer;
+	private XStream xstream;
 	
-	private ArrayList<String> tasks;
-	
+	/**
+	 * Constructor
+	 * 
+	 * 
+	 */
 	public Storage() {
 		this.initilize();
 	}
 	
-	public String save(ArrayList<Task> list) {
-		for (Task t: list) {
-			this.tasks.add(serialize(t));
+	/**
+	 * Save an TaskList object into the data file.
+	 * @param tasks the TaskList object which contains the list of tasks
+	 * @return a feedback message
+	 */
+	public String save(TaskList tasks) {
+		try {
+			this.writer = new BufferedWriter(new FileWriter(TASK_FILE, false));
+			this.writer.write(serialize(tasks));
+		} catch (IOException e) {
+			throw new Error(ERROR_IO);
 		}
-		
-		writeToFile(tasks);
-		
 		return "Success";
 	}
 	
+	/**
+	 * Load and build a TaskList object from the data file.
+	 * @return a TaskList object
+	 */
 	public TaskList load() {
-		return null;
+		String input = null;
+		StringBuilder xml = new StringBuilder(); 
+		try {
+			while ((input = reader.readLine()) != null) {
+				xml.append(input);
+			}
+		} catch (IOException e) {
+			throw new Error(ERROR_IO);
+		}
+//		System.out.println(xml.toString());
+		return (TaskList)xstream.fromXML(xml.toString());
 	}
 	
-	public String serialize(Task task) {
-		XStream xstream = new XStream();
-		xstream.alias("task", Task.class);
-		String xml = xstream.toXML(task);
+	/**
+	 * Serialize a TaskList object into xml string
+	 * @return xml format string
+	 */
+	public String serialize(TaskList tasks) {
+		String xml = xstream.toXML(tasks);
 		return xml;
 	}
 	
@@ -60,8 +84,11 @@ public class Storage {
 		}
 		
 	}
+	
+	/**
+	 * This method initialize all the file operators
+	 */
 	private void initilize() {
-		this.tasks = new ArrayList<>();
 		File inputFile = new File(TASK_FILE);
 		try {
 			if (inputFile.exists()) {
@@ -74,38 +101,44 @@ public class Storage {
 			}
 		} catch (Exception e) {
 			throw new Error(ERROR_IO);
-		}
+		}	
+		this.xstream = new XStream();
+		this.xstream.alias("CompleteList", TaskList.class);
+		this.xstream.alias("Task", Task.class);
+		this.xstream.processAnnotations(TaskList.class);
 	}
 	
-	
-	private void readFromFile() {
-		
-	}
-	
-	private void writeToFile(ArrayList<String> tasks) {
-		try {
-			this.writer = new BufferedWriter(new FileWriter(TASK_FILE, false));
-		} catch (IOException e1) {
-			throw new Error(ERROR_IO);
-		}
-		
-		for (String task : tasks) {
-			try {
-				this.writer.write(task);
-			} catch (Exception e) {
-				throw new Error(ERROR_IO);
-			}
-		}
-	}
 	
 	public static void main(String[] args) {
-		ArrayList<Task> test_input= new ArrayList<>();
-		test_input.add(new Task("IamHappy"));
-		test_input.add(new Task("Macbook"));
-		test_input.add(new Task("go to school tonight"));
-		System.out.println("aa");
+		testSave();
+		testLoad();
+	}
+
+	
+	/*************
+	 * Testing
+	 *************/
+	
+	
+	/**
+	 * Test save method
+	 */
+	public static void testSave() {
+		TaskList a = new TaskList();
+		a.test();
+		
 		Storage storge = new Storage();
-		storge.save(test_input);
+//		System.out.println(storge.serialize(a));
+		storge.save(a);
 		storge.close();
+	}
+	
+	/**
+	 * Test load method
+	 */
+	public static void testLoad() {
+		Storage storge = new Storage();
+		TaskList a = storge.load();
+		System.out.println(a.toString());
 	}
 }
