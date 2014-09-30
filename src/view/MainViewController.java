97c5ab9e0b52bd0242f1bf88b4376219061b0c30
Wang Zhipeng <ddmad1030@gmail.com>
2014-09-30 15:27:55 +0800

    Add set date function in view to display the date and time in GUI.

--- src/view/MainViewController.java
+++ src/view/MainViewController.java
@@ -1,7 +1,36 @@
 package view;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class MainViewController {
 	
+	@FXML
+	Label date;
+	
+	@FXML
+	VBox toBeCompleted;
+	
+	@FXML
+	VBox toDo;
+	
+	@FXML
+	TextField input;
+	
+	public void initialize() {
+		date.setText(getCurrentDate());
+	}
+	
+	private String getCurrentDate() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date currentDate = new Date();
+		return dateFormat.format(currentDate);
+	}
 	
 	
 }
