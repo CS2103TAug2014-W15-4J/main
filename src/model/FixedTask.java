4e57b5385d206126fb86eeaf0547c9a3ec655ed4
Jiang Sheng <gisonrg@gmail.com>
2014-10-07 19:45:01 +0800

    Closed #26. Update FixedTask model.

--- src/model/FixedTask.java
+++ src/model/FixedTask.java
@@ -1,10 +1,39 @@
 package model;
 
-public class FixedTask extends Task {
+import java.util.Date;
 
-	public FixedTask(String description) {
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+public class FixedTask extends Task {
+	
+	@XStreamAlias("Star Time")
+	private Date startTime;
+	@XStreamAlias("End Time")
+	private Date endTime;
+	
+	public FixedTask(String description, Date startTime, Date endTime) {
 		super(description);
 		// TODO Auto-generated constructor stub
+		this.startTime = startTime;
+		this.endTime = endTime;
 	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
+
 
 }
