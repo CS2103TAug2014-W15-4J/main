83b7d2bb855ae5d675f9d6bdd4d02f215e14ee80
Wang Zhipeng <ddmad1030@gmail.com>
2014-10-13 17:39:45 +0800

    Remove some documentation from MainView.java.

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -28,13 +28,11 @@ public class MainView extends Application {
 		mainViewControl = new MainViewController();
 		mainViewControl.setText("Enter Command here...");
 		mainViewControl.setDateLabel();
-//		mainViewControl.display();
-		
         
-//        Scene scene = new Scene(root, 434, 620);
+//      Scene scene = new Scene(root, 434, 620);
 		Scene scene = new Scene(mainViewControl);
-		primaryStage.setWidth(450); //<fx:root type="javafx.scene.layout.VBox" xmlns:fx="http://javafx.com/fxml"></fx:root>
-		primaryStage.setHeight(620); //<TextField fx:id="textField"/><Button text="Click Me" onAction="#doSomething"/>
+		primaryStage.setWidth(450); 
+		primaryStage.setHeight(620); 
         
         primaryStage.setTitle("uClear: Welcome!");
         primaryStage.setScene(scene);
