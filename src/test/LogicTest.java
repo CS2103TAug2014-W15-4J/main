951601a1d22f725b508117886180dbb606732dd7
Jiang Sheng <gisonrg@gmail.com>
2014-10-13 18:22:17 +0800

    Refactor the Test case for Logic

--- src/test/LogicTest.java
+++ src/test/LogicTest.java
@@ -8,6 +8,11 @@ import org.junit.Test;
 import controller.Logic;
 
 public class LogicTest {
+	final String MESSAGE_SUCCESS = "Task added successfully.";
+    final String MESSAGE_FAIL = "Invalid command. Type 'help' to see the list of available commands.";
+    final String MESSAGE_EMPTY = "Your task list is empty.";
+    final String COMMAND_SHOW = "show";
+    
     String feedback;
     String show;
     
@@ -17,33 +22,44 @@ public class LogicTest {
         Logic.initialize();
     }
     
-    @Test
-    public void testAdd() {
-        
-        // clears the task list
-        Logic.readAndExecuteCommands("clear");
-        
-        // test the adding of a floating task
-        feedback = Logic.readAndExecuteCommands("add testing floatingtask");
-        show = Logic.readAndExecuteCommands("show");
-        
-        assertEquals(feedback, "Task added successfully.");
-        assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n");
-        
-        // test the adding of a deadline task
-        feedback = Logic.readAndExecuteCommands("add testing by 13 Oct 2014 4pm");
-        show = Logic.readAndExecuteCommands("show"); 
-        
-        assertEquals(feedback, "Task added successfully.");
-        assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n");
-        
-        // test the adding of a fixed task
-        feedback = Logic.readAndExecuteCommands("add do this task on 14Oct 2013 4pm to 6pm");
-        show = Logic.readAndExecuteCommands("show");
-        
-        assertEquals(feedback, "Task added successfully.");
-        assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n\n3. do this task on 14Oct 2013\nStart: 2014-10-13 16:00\nDue: 2014-10-13 18:00\nStatus: Ongoing\n");
-    }
+@Test
+public void testAdd() {
+	final String MESSAGE_SUCCESS = "Task added successfully.";
+	final String MESSAGE_FAIL = "Invalid command. Type 'help' to see the list of available commands.";
+	final String MESSAGE_EMPTY = "Your task list is empty.";
+	final String COMMAND_SHOW = "show";
+	
+	// clears the task list
+	Logic.readAndExecuteCommands("clear");
+	
+	// test the adding nothing
+	feedback = Logic.readAndExecuteCommands("add");
+	show = Logic.readAndExecuteCommands(COMMAND_SHOW);
+	System.out.println(show);
+	assertEquals(feedback, MESSAGE_FAIL);
+	assertEquals(show, MESSAGE_EMPTY);
+	
+	// test the adding of a floating task
+	feedback = Logic.readAndExecuteCommands("add testing floatingtask");
+	show = Logic.readAndExecuteCommands(COMMAND_SHOW);
+	
+	assertEquals(feedback, MESSAGE_SUCCESS);
+	assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n");
+	
+	// test the adding of a deadline task
+	feedback = Logic.readAndExecuteCommands("add testing by 13 Oct 2014 4pm");
+	show = Logic.readAndExecuteCommands(COMMAND_SHOW); 
+	
+	assertEquals(feedback, MESSAGE_SUCCESS);
+	assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n");
+	
+	// test the adding of a fixed task
+	feedback = Logic.readAndExecuteCommands("add do this task on 14Oct 2013 4pm to 6pm");
+	show = Logic.readAndExecuteCommands(COMMAND_SHOW);
+	
+	assertEquals(feedback, MESSAGE_SUCCESS);
+	assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n\n3. do this task on 14Oct 2013\nStart: 2014-10-13 16:00\nDue: 2014-10-13 18:00\nStatus: Ongoing\n");
+}
     
     @Test
     public void testEdit() {
@@ -56,19 +72,19 @@ public class LogicTest {
         Logic.readAndExecuteCommands("add testing by 13 Oct 2014 4pm");
         Logic.readAndExecuteCommands("add do this task on 14Oct 2013 4pm to 6pm");
         
-        show = Logic.readAndExecuteCommands("show");
+        show = Logic.readAndExecuteCommands(COMMAND_SHOW);
         assertEquals(show, "Current tasks:\n1. testing floatingtask\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n\n3. do this task on 14Oct 2013\nStart: 2014-10-13 16:00\nDue: 2014-10-13 18:00\nStatus: Ongoing\n");
         
         // test the editing of description
         feedback = Logic.readAndExecuteCommands("edit 1 edited description");
-        show = Logic.readAndExecuteCommands("show");
+        show = Logic.readAndExecuteCommands(COMMAND_SHOW);
         
         assertEquals(feedback, "Task edited successfully.");
         assertEquals(show, "Current tasks:\n1. edited description\nStatus: Ongoing\n\n2. testing\nDue: 2014-10-13 16:00\nStatus: Ongoing\n\n3. do this task on 14Oct 2013\nStart: 2014-10-13 16:00\nDue: 2014-10-13 18:00\nStatus: Ongoing\n");
         
         // test the editing of time for deadline task
         feedback = Logic.readAndExecuteCommands("edit 2 8 October 2013 3pm");
-        show = Logic.readAndExecuteCommands("show");
+        show = Logic.readAndExecuteCommands(COMMAND_SHOW);
         
         assertEquals(feedback, "Task edited successfully.");
         assertEquals(show, "Current tasks:\n1. edited description\nStatus: Ongoing\n\n2. testing\nDue: 2013-10-08 15:00\nStatus: Ongoing\n\n3. do this task on 14Oct 2013\nStart: 2014-10-13 16:00\nDue: 2014-10-13 18:00\nStatus: Ongoing\n");
