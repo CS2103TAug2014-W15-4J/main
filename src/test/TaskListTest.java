78c9c2c8f8164a638123e255be795fc9b7849db0
Jiang Sheng <gisonrg@gmail.com>
2014-10-15 21:44:30 +0800

    close #63. Add a new test for checking the queue generate by PrepareDisplay method.

--- src/test/TaskListTest.java
+++ src/test/TaskListTest.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Queue;
 
 import model.FixedTask;
 import model.FloatingTask;
+import model.Task;
 import model.TaskList;
 
 import org.junit.BeforeClass;
@@ -16,11 +18,6 @@ import org.junit.Test;
 import exception.TaskInvalidDateException;
 
 public class TaskListTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
 	@Test
 	public void testAddingTask() {
 		TaskList tasks = new TaskList();
@@ -67,6 +64,7 @@ public class TaskListTest {
 		assertEquals(0, tasks.count());
 	}
 	
+	@Test
 	public void testEditingTask() throws TaskInvalidDateException {
 		TaskList tasks = new TaskList();
 		// editing floating task
@@ -82,6 +80,39 @@ public class TaskListTest {
 		tasks.addToList("lowercase", dateA, dateB);
 		tasks.editTaskStartDate(1, dateC);
 		assertEquals(dateC, tasks.get(0).getDeadline());
+	}
+	
+	@Test
+	public void testPrepareDisplayList() {
+		
+		
+		TaskList tasks = new TaskList();
+		tasks.addToList("Late", new Date(100), new Date(1000));
+		pause(200); // wait for 0.2s to add the next one
+		tasks.addToList("Early", new Date(200));
+		pause(200); // wait for 0.2s to add the next one
+		tasks.addToList("Middle", new Date(500));
+		
+		// Test order by addedTime
+		Queue<Task> result = tasks.prepareDisplayList(true);
+		assertEquals("Late", result.poll().getDescription());
+		assertEquals("Early", result.poll().getDescription());
+		assertEquals("Middle", result.poll().getDescription());
+		
+		// Test order by addedTime
+		result = tasks.prepareDisplayList(false);
+		assertEquals("Early", result.poll().getDescription());
+		assertEquals("Middle", result.poll().getDescription());
+		assertEquals("Late", result.poll().getDescription());
+		
+	}
+	
+	void pause(int ms) {
+		try {
+		    Thread.sleep(ms); 
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
 	}
 
 }
