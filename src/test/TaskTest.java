f90647a2615cef1bb73bdf76716114b9c76da54b
Jiang Sheng <gisonrg@gmail.com>
2014-11-05 22:21:58 +0800

    Update testing for task models.

--- src/test/TaskTest.java
+++ src/test/TaskTest.java
@@ -9,17 +9,41 @@ import model.DeadlineTask;
 import model.FixedTask;
 import model.FloatingTask;
 import model.RepeatedTask;
-import model.Task;
 import model.Task.Type;
-
 import controller.UserInput.RepeatDate;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import exception.TaskDoneException;
 import exception.TaskTagDuplicateException;
 
 public class TaskTest {
-
+	static Date today;
+	static Date tomorrow;
+	static Date theDayAfterTomorrow;
+	static Date nextWeekOfTomorrow;
+	static Date nextMonthOfTomorrow;
+	
+	@BeforeClass
+	public static void setUpTime() {
+		today = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(today);
+		cal.add(Calendar.DATE, 1);
+		tomorrow = cal.getTime();
+		cal.add(Calendar.DATE, 1);
+		theDayAfterTomorrow = cal.getTime();
+		
+		cal.setTime(tomorrow);
+		cal.add(Calendar.DATE, 7);
+		nextWeekOfTomorrow = cal.getTime();
+		cal.setTime(tomorrow);
+		cal.add(Calendar.MONTH, 1);
+		nextMonthOfTomorrow = cal.getTime();
+	}
+	
+	
 	@Test
 	public void testFloatingTask() throws TaskTagDuplicateException {
 		FloatingTask task = new FloatingTask("This is a floating task");
@@ -32,42 +56,80 @@ public class TaskTest {
 		assertEquals("Description edited", task.getDescription());
 		
 		// test tagging
-		testTags(task);
+		// add tag
+		task.addTag("Tag1");
+		task.addTag("Tag2");
+		task.addTag("Tag3");
+		
+		// test existing tags
+		assertTrue(task.getTags().contains("tag1"));
+		assertTrue(task.getTags().contains("tag2"));
+		assertTrue(task.getTags().contains("tag3"));
+		
+		// test non-existing tags
+		assertFalse(task.getTags().contains("tag4"));
+		assertFalse(task.getTags().contains("tag5"));
+		
+		// test adding same tag
+		try {
+			task.addTag("Tag1");
+		} catch (TaskTagDuplicateException e) {
+			assertTrue(e instanceof TaskTagDuplicateException);
+		}
 	}
 
 	@Test
 	public void testFixedTask() throws TaskTagDuplicateException {
-		Date start = new Date(100);
-		Date end = new Date(200);
-		FixedTask task = new FixedTask("This is a Fixed task", start, end);
+		FixedTask task = new FixedTask("This is a Fixed task", today, tomorrow);
 		assertEquals(Type.FIXED ,task.getType());
 		assertEquals("This is a Fixed task", task.getDescription());
 		assertFalse(task.getIsDone());
-		
+				
 		// test edit description
 		task.setDescription("Description edited");
 		assertEquals("Description edited", task.getDescription());
 		
-		assertEquals(start, task.getStartTime());
-		assertEquals(end, task.getDeadline());
+		assertEquals(today, task.getStartTime());
+		assertEquals(tomorrow, task.getDeadline());
 		
-		// test edit time
-		Date newStart = new Date(100);
-		Date newEnd = new Date(100);
+		// test remaining days
+		assertEquals(1, task.getReminingDays());
 		
-		task.setStartTime(newStart);
-		assertEquals(newStart, task.getStartTime());
-		task.setDeadline(newEnd);
-		assertEquals(newEnd, task.getDeadline());
+		task.setStartTime(tomorrow);
+		assertEquals(tomorrow, task.getStartTime());
+		task.setDeadline(theDayAfterTomorrow);
+		assertEquals(theDayAfterTomorrow, task.getDeadline());
+		
+		
+		// test remaining days
+		assertEquals(2, task.getReminingDays());
 		
 		// test tagging
-		testTags(task);
+		// add tag
+		task.addTag("Tag1");
+		task.addTag("Tag2");
+		task.addTag("Tag3");
+		
+		// test existing tags
+		assertTrue(task.getTags().contains("tag1"));
+		assertTrue(task.getTags().contains("tag2"));
+		assertTrue(task.getTags().contains("tag3"));
+		
+		// test non-existing tags
+		assertFalse(task.getTags().contains("tag4"));
+		assertFalse(task.getTags().contains("tag5"));
+		
+		// test adding same tag
+		try {
+			task.addTag("Tag1");
+		} catch (TaskTagDuplicateException e) {
+			assertTrue(e instanceof TaskTagDuplicateException);
+		}
 	}
 	
 	@Test
 	public void testDeadlineTask() throws TaskTagDuplicateException {
-		Date end = new Date(200);
-		DeadlineTask task = new DeadlineTask("This is a Deadline task", end);
+		DeadlineTask task = new DeadlineTask("This is a Deadline task", tomorrow);
 		assertEquals(Type.DEADLINE ,task.getType());
 		assertEquals("This is a Deadline task", task.getDescription());
 		assertFalse(task.getIsDone());
@@ -76,59 +138,32 @@ public class TaskTest {
 		task.setDescription("Description edited");
 		assertEquals("Description edited", task.getDescription());
 		
-		assertEquals(end, task.getDeadline());
+		assertEquals(tomorrow, task.getDeadline());
+		
+		// test remaining days
+		assertEquals(1, task.getReminingDays());
 		
 		// test edit time
-		Date newEnd = new Date(100);
+		task.setDeadline(theDayAfterTomorrow);
+		assertEquals(theDayAfterTomorrow, task.getDeadline());
 		
-		task.setDeadline(newEnd);
-		assertEquals(newEnd, task.getDeadline());
-		
+		// test remaining days after changing time
+		assertEquals(2, task.getReminingDays());
+				
 		// test tagging
-		testTags(task);
-	}
-	
-	@Test
-	public void testRepeatedTask() {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(new Date());
-		cal.add(Calendar.DAY_OF_YEAR, 1);
-		Date tomorrow = cal.getTime();
-		
-		// DAILY task
-		
-		// set deadline to be tomorrow
-		RepeatedTask task = new RepeatedTask("This is a Deadline task", tomorrow, RepeatDate.DAILY);
-		assertEquals(Type.REPEATED ,task.getType());
-		assertEquals("This is a Deadline task", task.getDescription());
-		assertFalse(task.getIsDone());
-		
-		assertEquals(tomorrow, task.getDeadline());
-	}
-
-	/**
-	 * @param task
-	 * @throws TaskTagDuplicateException
-	 */
-	private void testTags(Task task) throws TaskTagDuplicateException {
 		// add tag
 		task.addTag("Tag1");
 		task.addTag("Tag2");
 		task.addTag("Tag3");
-		task.addTag("Tag4");
-		task.addTag("Tag5");
 		
 		// test existing tags
 		assertTrue(task.getTags().contains("tag1"));
 		assertTrue(task.getTags().contains("tag2"));
 		assertTrue(task.getTags().contains("tag3"));
-		assertTrue(task.getTags().contains("tag4"));
-		assertTrue(task.getTags().contains("tag5"));
 		
 		// test non-existing tags
-		assertFalse(task.getTags().contains("tag6"));
-		assertFalse(task.getTags().contains("tag7"));
-		assertFalse(task.getTags().contains("tag8"));
+		assertFalse(task.getTags().contains("tag4"));
+		assertFalse(task.getTags().contains("tag5"));
 		
 		// test adding same tag
 		try {
@@ -136,6 +171,80 @@ public class TaskTest {
 		} catch (TaskTagDuplicateException e) {
 			assertTrue(e instanceof TaskTagDuplicateException);
 		}
+	}
+	
+	@Test
+	public void testRepeatedTask() throws TaskDoneException {	
+		// DAILY task
+		
+		// set deadline to be tomorrow
+		RepeatedTask task = new RepeatedTask("This is a Repeated task", tomorrow, RepeatDate.DAILY);
+		assertEquals(Type.REPEATED ,task.getType());
+		assertEquals("This is a Repeated task", task.getDescription());
+		assertFalse(task.getIsDone());
+		
+		// test edit description
+		task.setDescription("Description edited");
+		assertEquals("Description edited", task.getDescription());
+		
+		assertEquals(tomorrow, task.getDeadline());
+		
+		// test remaining days
+		assertEquals(1, task.getReminingDays());
+		
+		// check occurrence
+		
+		// done this task, check if it return a new task for the next repeat period
+		RepeatedTask nextTask = (RepeatedTask) task.markDone();
+		assertEquals(theDayAfterTomorrow, nextTask.getDeadline());
+		
+		// Weekly task
+		
+		// set deadline to be tomorrow
+		task = new RepeatedTask("This is a Repeated task", tomorrow, RepeatDate.WEEKLY);
+		assertEquals(Type.REPEATED ,task.getType());
+		assertEquals("This is a Repeated task", task.getDescription());
+		assertFalse(task.getIsDone());
+		
+		// test edit description
+		task.setDescription("Description edited");
+		assertEquals("Description edited", task.getDescription());
+		
+		assertEquals(tomorrow, task.getDeadline());
+		
+		// test remaining days
+		assertEquals(1, task.getReminingDays());
+		
+		// check occurrence
+		
+		// done this task, check if it return a new task for the next repeat period
+		nextTask = (RepeatedTask) task.markDone();
+		assertEquals(nextWeekOfTomorrow, nextTask.getDeadline());
+		
+		// Weekly task
+		
+		// set deadline to be tomorrow
+		task = new RepeatedTask("This is a Repeated task", tomorrow, RepeatDate.MONTHLY);
+		assertEquals(Type.REPEATED ,task.getType());
+		assertEquals("This is a Repeated task", task.getDescription());
+		assertFalse(task.getIsDone());
+		
+		// test edit description
+		task.setDescription("Description edited");
+		assertEquals("Description edited", task.getDescription());
+		
+		assertEquals(tomorrow, task.getDeadline());
+		
+		// test remaining days
+		assertEquals(1, task.getReminingDays());
+		
+		// check occurrence
+		
+		// done this task, check if it return a new task for the next repeat period
+		nextTask = (RepeatedTask) task.markDone();
+		assertEquals(nextMonthOfTomorrow, nextTask.getDeadline());
+		
+		
 	}
 
 }
